6a62efa5a0cb1be8d4a399f159d3786f6753f42d
ylcnkyrabia <ry837@nyu.edu>
2018-04-16 19:00:31 -0400

    Update CCHomeWork.js

--- CCHomeWork.js
+++ CCHomeWork.js
@@ -1,21 +1,19 @@
 var sketch1= function(p){
 	
 	p.setup = function () {
-        p.createCanvas(1000,400);
             var istanbul = p.createImg("https://media.giphy.com/media/joX2vj9TLEjxC/source.gif");
             istanbul.position(0,0);
-            istanbul.size(p.width, p.height/3.2);
+            istanbul.size(200, 100);
 
     };
 };
-var myp5 = new p5(sketch1);
+var myp5 = new p5(sketch1, "istanbul");
 
 var sketch2 = function(p) {
     p.setup = function (){
-        p.createCanvas(400,400);
-        var tower = p.createImage("https://media.giphy.com/media/7d5Oh7q6zeKWY/source.gif");
-        tower.position(0, 300);
-        tower.size(720, 266);        
+        var tower = p.createImg("https://media.giphy.com/media/7d5Oh7q6zeKWY/source.gif");
+        tower.position(30, 700);
+        tower.size(420, 166);        
         
     };    
 };
@@ -24,7 +22,7 @@ var myp5 = new p5(sketch2);
 
 var sketch3 = function (p){
     p.preload = function(){
-    p.song = loadSound("https://www.youtube.com/watch?v=W7aIdw-drtM");
+    p.song = p.loadSound("Mercan Dede - Istanbul.mp3");
         
     };
     
